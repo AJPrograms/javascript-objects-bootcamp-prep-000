c17477003275ad431c859f8b780a21e4c2207abd
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 03:06:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var meals = {};
 var meals = new Object();
 var meals = { breakfast: "oatmeal" };
 var meals = new Object({ breakfast: 'oatmeal' });
+var meals = {
+  breakfast: 'avocado',
+  lunch: 'avocado',
+  dinner: 'avocado'
+}
