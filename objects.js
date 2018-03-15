9d4bcd52d930eb5ece467286d1bd7d8fcdeb9b8b
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 03:07:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,10 @@ var meals = new Object();
 var meals = { breakfast: "oatmeal" };
 var meals = new Object({ breakfast: 'oatmeal' });
 var meals = {
+  breakfast: 'eggs',
+  breakfast: 'bacon'
+}
+var meals = {
   breakfast: 'avocado',
   lunch: 'avocado',
   dinner: 'avocado'
